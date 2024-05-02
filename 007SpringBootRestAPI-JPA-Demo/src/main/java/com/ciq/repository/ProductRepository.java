4c7196b3dbc571f1d6deecbb9cb7a8428661570a
TharunReddy372 <165243436+TharunReddy372@users.noreply.github.com>
2024-05-02 11:19:12 +0530

    Add files via upload

---
+++ 007SpringBootRestAPI-JPA-Demo/src/main/java/com/ciq/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.ciq.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ciq.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+	
+
+}
