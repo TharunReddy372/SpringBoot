048cde27fae16b16078b5a39d72398950b5b1a3c
TharunReddy372 <165243436+TharunReddy372@users.noreply.github.com>
2024-05-02 11:11:28 +0530

    Add files via upload

---
+++ 003SpringBootJDBC-MVC-DEMO/src/main/java/com/ciq/service/StudentServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ciq.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ciq.dao.StudentDAO;
+import com.ciq.model.Student;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+	
+	@Autowired
+	private StudentDAO studentDAO;
+
+	@Override
+	public void add(Student student) {
+		studentDAO.add(student);
+	}
+
+	@Override
+	public void update(Student student) {
+		studentDAO.update(student);
+	}
+
+	@Override
+	public List<Student> findAll() {
+		return studentDAO.findAll();
+	}
+
+	@Override
+	public void deleteById(Integer sid) {
+		studentDAO.deleteById(sid);
+	}
+
+	@Override
+	public Student findById(Integer sid) {
+		return studentDAO.findById(sid);
+	}
+
+}
