d68b3538730acc0572268fdb80ea34852d414d24
TharunReddy372 <165243436+TharunReddy372@users.noreply.github.com>
2024-05-09 16:54:37 +0530

    Add files via upload

---
+++ 007SpringBoot-Consume-Rest-API-Demo-1/src/main/java/com/ciq/service/ProductApiConsumer.java
@@ -0,0 +1,11 @@
+package com.ciq.service;
+
+import java.util.List;
+
+import com.ciq.model.Product;
+
+public interface ProductApiConsumer {
+	
+	public List<Product> getProducts();
+
+}
---
+++ 007SpringBoot-Consume-Rest-API-Demo-1/src/main/java/com/ciq/service/ProductApiConsumerImpl.java
@@ -0,0 +1,29 @@
+package com.ciq.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.ciq.model.Product;
+import com.ciq.model.ResponseObj;
+
+@Service
+public class ProductApiConsumerImpl implements ProductApiConsumer {
+
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Override
+	public List<Product> getProducts() {
+		
+		String url = "localhost:9090/products";
+		
+		ResponseObj responseObj = restTemplate.getForObject(url, ResponseObj.class);
+		
+		
+		return (List<Product>)responseObj.getResult();
+	}
+
+}
